0872b00b0241ff512e1318b9bcac051b607e3f2b
Sumit Heda <sumit@habilelabs.io>
2018-07-21 16:07:19 +0530

    Changes: for the html spec reporter.

--- test/e2e.conf.js
+++ test/e2e.conf.js
@@ -1,4 +1,5 @@
-let SpecReporter = require('jasmine-spec-reporter').SpecReporter;
+let HTMLReport = require('protractor-html-reporter');
+let jasmineReporters = require('jasmine-reporters');
 
 exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
@@ -16,10 +17,29 @@ exports.config = {
         }
     },
     onPrepare: function () {
-        jasmine.getEnv().addReporter(new SpecReporter({
-            spec: {
-                displayStacktrace: false
-            }
+        jasmine.getEnv().addReporter(new jasmineReporters.JUnitXmlReporter({
+            consolidateAll: true,
+            savePath: './',
+            filePrefix: 'xmlresults'
         }));
+    },
+    //HTMLReport called once tests are finished
+    onComplete: function() {
+        let browserName, browserVersion;
+        let capsPromise = browser.getCapabilities();
+        capsPromise.then(function (caps) {
+            browserName = caps.get('browserName');
+            browserVersion = caps.get('version');
+            testConfig = {
+                reportTitle: 'Test Execution Report',
+                outputPath: './',
+                screenshotPath: './screenshots',
+                testBrowser: browserName,
+                browserVersion: browserVersion,
+                modifiedSuiteName: false,
+                screenshotsOnlyOnFailure: true
+            };
+            new HTMLReport().from('xmlresults.xml', testConfig);
+        });
     }
 };
